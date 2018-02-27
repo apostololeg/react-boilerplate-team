a18e696bcd89d53b267dc78ab4f4c227ba094515
Eugene Gavrilov <eugene-gavriloff@yandex.ru>
2018-02-27 20:39:11 +0300

    Added flags fro css-modules and html-webpack-plugin

--- config/tools/constants.js
+++ config/tools/constants.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 // Node ENV ("production", "development", etc.)
 const NODE_ENV = process.env.NODE_ENV;
 
+// Flag determining whether to use html
+const USE_HTML = (env.USE_HTML || true);
+
 // Application title
 const APP_TITLE = env.APP_TITLE || 'React boilerplate';
 
@@ -22,17 +25,24 @@ const ASSETS_PATH = env.ASSETS_PATH || 'src/assets';
 // Application's page language
 const PAGE_LANG = env.PAGE_LANG || 'en';
 
-const HOST = env.HOST || 'localhost';
+// The flag that determines whether to use the css modules
+const CSS_MODULES = env.CSS_MODULES || false;
 
+/**
+ * Application server options
+ */
+const HOST = env.HOST || 'localhost';
 const PORT = env.PORT || 9000;
 
 module.exports = {
   NODE_ENV,
+  USE_HTML,
   APP_TITLE,
   APP_PATH,
   BUILD_PATH,
   SOURCE_PATH,
   ASSETS_PATH,
+  CSS_MODULES,
   PAGE_LANG,
   HOST,
   PORT
