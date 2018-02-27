04a3b9e5da4674dba2dbf730ad62697b0df3f36f
Eugene Gavrilov <eugene-gavriloff@yandex.ru>
2018-02-27 20:39:27 +0300

    Removed SugarSS

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -2,11 +2,8 @@ const { NODE_ENV } = require('./tools/constants');
 
 module.exports = {
   exec: true,
-  parser: require('sugarss'),
   sourceMap: (NODE_ENV === 'production' ? false : 'inline'),
   plugins: [
-    require('postcss-import')(),
-    require('postcss-modules-values')(),
     (NODE_ENV === 'production') && require('cssnano')({
       autoprefixer: true,
       preset: ['default', {
